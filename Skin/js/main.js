be19f17c32ea815254b1c886b28c6635796aaec1
Shao <leo21604212@gmail.com>
2023-11-16 13:07:02 -0500

    678

--- Skin/js/main.js
+++ Skin/js/main.js
@@ -1,1 +1,35 @@
 import "../css/style.css";
+const DOMSelectors = {
+    form: document.querySelector("#form"),
+    ChampName: document.querySelector(".Champ-Name"),
+    ChampImage: document.querySelector("#ChampImage"),
+    ChampTitle: document.querySelector("#ChampTitle"),
+    gallery: document.querySelector("#gallery")
+};
+
+function card(Champ) {
+    DOMSelectors.gallery.insertAdjacentHTML(
+        "beforeend",
+        `<div id="gall">
+            <h2 class="card-title">${Champ.ChampName.value}</h2>
+            <img src="${Champ.ChampImage.value}" alt="Champion Image" class="card-image"/>
+            <h3 class="card-price">${Champ.ChampTitle.value}</h3>
+            <button>dies</button>
+        </div>`
+    );
+};
+
+function remove(){
+    let buttons = document.querySelectorAll("button")
+    buttons.forEach((btn)=>
+    btn.addEventListener("click", function(event){
+        btn.parentElement.remove();
+    })
+    );
+}
+DOMSelectors.form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    card(DOMSelectors);
+    clearFields();
+    remove();
+});
