dc54e9f200ec4922f62da0f27d86c732ac757a5f
Shao <leo21604212@gmail.com>
2023-11-21 13:11:17 -0500

    qwe

--- Skin/js/main.js
+++ Skin/js/main.js
@@ -1,35 +1,46 @@
 import "../css/style.css";
-const DOMSelectors = {
-    form: document.querySelector("#form"),
-    ChampName: document.querySelector(".Champ-Name"),
-    ChampImage: document.querySelector("#ChampImage"),
-    ChampTitle: document.querySelector("#ChampTitle"),
-    gallery: document.querySelector("#gallery")
-};
+import {Lux} from "./skins.js";
 
-function card(Champ) {
-    DOMSelectors.gallery.insertAdjacentHTML(
+const DOMSelectors = {
+    ChampName: document.querySelector(".SkinName"),
+    ChampIMG: document.querySelector(".img"),
+    ChampPrice: document.querySelector(".Price"),
+    ChampOp: document.querySelector(".MyOpinion"),
+    PasP: document.querySelector(".PPink"),
+    PasB: document.querySelector(".PBlue"),
+};
+Lux.forEach((Champ) => {
+ DOMSelectors.gallery.insertAdjacentHTML(
         "beforeend",
-        `<div id="gall">
-            <h2 class="card-title">${Champ.ChampName.value}</h2>
-            <img src="${Champ.ChampImage.value}" alt="Champion Image" class="card-image"/>
-            <h3 class="card-price">${Champ.ChampTitle.value}</h3>
+        `<div id="gallery">
+            <h2 class="SkinName">${Champ.ChampName.value}</h2>
+            <img src="${Champ.ChampIMG.value}" alt="Champion Image" class="img"/>
+            <h3 class="Price">${Champ.ChampPrice.value}</h3>
+            <h3 class="MyOpinion">${Champ.ChampOP.value}</h3>
             <button>dies</button>
         </div>`
-    );
-};
+ )});
 
-function remove(){
+
+function pas(){
+
+};
+DOMSelectors.form.addEventListener("click", function(event){
+
+});
+
+
+/* function remove(){
     let buttons = document.querySelectorAll("button")
     buttons.forEach((btn)=>
     btn.addEventListener("click", function(event){
         btn.parentElement.remove();
     })
-    );
-}
-DOMSelectors.form.addEventListener("submit", function(event) {
+    ); */
+
+/* DOMSelectors.form.addEventListener("submit", function(event) {
     event.preventDefault();
     card(DOMSelectors);
     clearFields();
     remove();
-});
+}); */
--- Skin/js/skins.js
+++ Skin/js/skins.js
@@ -3,8 +3,62 @@ const Lux =[
     SkinName:"Spellthief Lux",
     Price: "950RP",
     MyOpinion:"2.5/10",
-    img:""
-}
-
+    img:"",
+},
+{
+    SkinName:"Cosmic Lux",
+    Price: "1820RP",
+    MyOpinion:"10/10",
+    img:"",
+},
+{
+    SkinName:"Dark Cosmic Lux",
+    Price: "1820RP",
+    MyOpinion:"9.2/10",
+    img:"",
+},
+{
+    SkinName:"Elementalist Lux",
+    Price: "3250RP",
+    MyOpinion:"10/10",
+    img:"",
+},
+{
+    SkinName:"Soul Fighter Lux",
+    Price: "1350RP",
+    MyOpinion:"7.4/10",
+    img:"",
+},
+{
+    SkinName:"Star Guardian Lux",
+    Price: "1350RP",
+    MyOpinion:"6.5/10",
+    img:"",
+},
+{
+    SkinName:"Steel Legion Lux",
+    Price: "950RP",
+    MyOpinion:"3/10",
+    img:"",
+},
+{
+    SkinName:"Pajama Guardian Lux",
+    Price: "1350RP",
+    MyOpinion:"7/10",
+    img:"",
+},
+{
+    SkinName:"Battle Academia Lux",
+    Price: "1350RP",
+    MyOpinion:"5/10",
+    img:"",
+},
+{
+    SkinName:"Prestige Battle Academia Lux",
+    Price: "125ME",
+    MyOpinion:"9.5/10",
+    img:"",
+},
 ]
 
+export {Lux}
